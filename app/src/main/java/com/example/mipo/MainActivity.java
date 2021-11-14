9d2fd1bf7535b6294a19b3a9aa0cc6ebe9e2d60a
Hardik Sharma <hardik00051@gmail.com>
2021-11-14 16:24:51 +0530

    Added add expense fragment to main activity

--- app/src/main/java/com/example/mipo/MainActivity.java
+++ app/src/main/java/com/example/mipo/MainActivity.java
@@ -4,18 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //progress bar
         CircularProgressBar circularProgressBar = (CircularProgressBar) findViewById(R.id.circularProgress);
         circularProgressBar.setProgress(50);
         circularProgressBar.setProgressColor(Color.CYAN);
         circularProgressBar.setTextColor(Color.BLACK);
+
+        fab = findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddExpense.newInstance().show(getSupportFragmentManager(), AddExpense.TAG);
+            }
+        });
+
+
+
 
     }
 }
